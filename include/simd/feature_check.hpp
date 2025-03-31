02a4458262fe3a3e1378f8bc98ed31c8f095ef22
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-03-31 15:13:14 +0300

    Add highest_feature function to determine the most advanced SIMD feature available

--- include/simd/feature_check.hpp
+++ include/simd/feature_check.hpp
@@ -1734,6 +1734,111 @@ static constexpr bool has() noexcept
         return false;
 }
 
+constexpr Feature highest_feature() noexcept
+{
+    if (has<Feature::CET_SS>())
+        return Feature::CET_SS;
+    if (has<Feature::CET_IBT>())
+        return Feature::CET_IBT;
+    if (has<Feature::SGX>())
+        return Feature::SGX;
+    if (has<Feature::RDPID>())
+        return Feature::RDPID;
+    if (has<Feature::GFNI>())
+        return Feature::GFNI;
+    if (has<Feature::AVX512_4FMAPS>())
+        return Feature::AVX512_4FMAPS;
+    if (has<Feature::AVX512_4VNNIW>())
+        return Feature::AVX512_4VNNIW;
+    if (has<Feature::PREFETCHWT1>())
+        return Feature::PREFETCHWT1;
+    if (has<Feature::ADX>())
+        return Feature::ADX;
+    if (has<Feature::RDSEED>())
+        return Feature::RDSEED;
+    if (has<Feature::RDRND>())
+        return Feature::RDRND;
+    if (has<Feature::SHA>())
+        return Feature::SHA;
+    if (has<Feature::VPCLMULQDQ>())
+        return Feature::VPCLMULQDQ;
+    if (has<Feature::PCLMULQDQ>())
+        return Feature::PCLMULQDQ;
+    if (has<Feature::VAES>())
+        return Feature::VAES;
+    if (has<Feature::AES>())
+        return Feature::AES;
+    if (has<Feature::AMX_BF16>())
+        return Feature::AMX_BF16;
+    if (has<Feature::AMX_INT8>())
+        return Feature::AMX_INT8;
+    if (has<Feature::AMX_TILE>())
+        return Feature::AMX_TILE;
+    if (has<Feature::AVX512FP16>())
+        return Feature::AVX512FP16;
+    if (has<Feature::AVX512BF16>())
+        return Feature::AVX512BF16;
+    if (has<Feature::AVX512VP2INTERSECT>())
+        return Feature::AVX512VP2INTERSECT;
+    if (has<Feature::AVX512VPOPCNTDQ>())
+        return Feature::AVX512VPOPCNTDQ;
+    if (has<Feature::AVX512BITALG>())
+        return Feature::AVX512BITALG;
+    if (has<Feature::AVX512VNNI>())
+        return Feature::AVX512VNNI;
+    if (has<Feature::AVX512VBMI2>())
+        return Feature::AVX512VBMI2;
+    if (has<Feature::AVX512VBMI>())
+        return Feature::AVX512VBMI;
+    if (has<Feature::AVX512IFMA>())
+        return Feature::AVX512IFMA;
+    if (has<Feature::AVX512VL>())
+        return Feature::AVX512VL;
+    if (has<Feature::AVX512BW>())
+        return Feature::AVX512BW;
+    if (has<Feature::AVX512DQ>())
+        return Feature::AVX512DQ;
+    if (has<Feature::AVX512CD>())
+        return Feature::AVX512CD;
+    if (has<Feature::AVX512F>())
+        return Feature::AVX512F;
+    if (has<Feature::MOVBE>())
+        return Feature::MOVBE;
+    if (has<Feature::BMI2>())
+        return Feature::BMI2;
+    if (has<Feature::BMI1>())
+        return Feature::BMI1;
+    if (has<Feature::LZCNT>())
+        return Feature::LZCNT;
+    if (has<Feature::POPCNT>())
+        return Feature::POPCNT;
+    if (has<Feature::F16C>())
+        return Feature::F16C;
+    if (has<Feature::FMA>())
+        return Feature::FMA;
+    if (has<Feature::AVX2>())
+        return Feature::AVX2;
+    if (has<Feature::AVX>())
+        return Feature::AVX;
+    if (has<Feature::SSE42>())
+        return Feature::SSE42;
+    if (has<Feature::SSE41>())
+        return Feature::SSE41;
+    if (has<Feature::SSSE3>())
+        return Feature::SSSE3;
+    if (has<Feature::SSE3>())
+        return Feature::SSE3;
+    if (has<Feature::SSE2>())
+        return Feature::SSE2;
+    if (has<Feature::SSE>())
+        return Feature::SSE;
+    if (has<Feature::MMX>())
+        return Feature::MMX;
+    return Feature::NONE;
+}
+
+constexpr Feature max_feature = highest_feature();
+
 } // namespace compile_time
 
 inline int get_simd_support() { return 5; }
