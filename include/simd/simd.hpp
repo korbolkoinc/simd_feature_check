50914080f973142f9e03af5b8f6a0c0690d59ea8
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-04-03 22:09:01 +0300

    Add insert method for SIMD vector to modify elements by index

--- include/simd/simd.hpp
+++ include/simd/simd.hpp
@@ -1471,7 +1471,7 @@ struct vector_ops<T, N, std::enable_if_t<simd::FeatureDetector<simd::Feature::SS
             return tmp[index % 4];
 #endif
         }
-        
+
         else if constexpr (std::is_same_v<T, int64_t> || std::is_same_v<T, uint64_t>)
         {
 #if SIMD_SSE4_1
@@ -1480,6 +1480,64 @@ struct vector_ops<T, N, std::enable_if_t<simd::FeatureDetector<simd::Feature::SS
             alignas(16) T tmp[2];
             _mm_store_si128(reinterpret_cast<__m128i*>(tmp), *src);
             return tmp[index % 2];
+#endif
+        }
+    }
+
+    static SIMD_INLINE void insert(register_t* dst, size_t index, T value)
+    {
+        if constexpr (std::is_same_v<T, float>)
+        {
+            alignas(16) float tmp[4];
+            _mm_store_ps(tmp, *dst);
+            tmp[index % 4] = value;
+            *dst = _mm_load_ps(tmp);
+        }
+        else if constexpr (std::is_same_v<T, double>)
+        {
+            alignas(16) double tmp[2];
+            _mm_store_pd(tmp, *dst);
+            tmp[index % 2] = value;
+            *dst = _mm_load_pd(tmp);
+        }
+        else if constexpr (std::is_same_v<T, int8_t> || std::is_same_v<T, uint8_t>)
+        {
+#if SIMD_SSE4_1
+            *dst = _mm_insert_epi8(*dst, static_cast<int>(value), index % 16);
+#else
+            alignas(16) int8_t tmp[16];
+            _mm_store_si128(reinterpret_cast<__m128i*>(tmp), *dst);
+            tmp[index % 16] = value;
+            *dst = _mm_load_si128(reinterpret_cast<const __m128i*>(tmp));
+#endif
+        }
+        else if constexpr (std::is_same_v<T, int16_t> || std::is_same_v<T, uint16_t>)
+        {
+            alignas(16) int16_t tmp[8];
+            _mm_store_si128(reinterpret_cast<__m128i*>(tmp), *dst);
+            tmp[index % 8] = static_cast<int16_t>(value);
+            *dst = _mm_load_si128(reinterpret_cast<const __m128i*>(tmp));
+        }
+        else if constexpr (std::is_same_v<T, int32_t> || std::is_same_v<T, uint32_t>)
+        {
+#if SIMD_SSE4_1
+            *dst = _mm_insert_epi32(*dst, static_cast<int>(value), index % 4);
+#else
+            alignas(16) int32_t tmp[4];
+            _mm_store_si128(reinterpret_cast<__m128i*>(tmp), *dst);
+            tmp[index % 4] = static_cast<int32_t>(value);
+            *dst = _mm_load_si128(reinterpret_cast<const __m128i*>(tmp));
+#endif
+        }
+        else if constexpr (std::is_same_v<T, int64_t> || std::is_same_v<T, uint64_t>)
+        {
+#if SIMD_SSE4_1
+            *dst = _mm_insert_epi64(*dst, static_cast<long long>(value), index % 2);
+#else
+            alignas(16) int64_t tmp[2];
+            _mm_store_si128(reinterpret_cast<__m128i*>(tmp), *dst);
+            tmp[index % 2] = static_cast<int64_t>(value);
+            *dst = _mm_load_si128(reinterpret_cast<const __m128i*>(tmp));
 #endif
         }
     }
