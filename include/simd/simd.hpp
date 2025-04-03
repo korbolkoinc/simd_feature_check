4c2d0d4e3d7ddabda68a221ef6b24e50f6a23486
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-04-03 12:16:28 +0300

    Add SIMD versioning and type concepts for SIMD arithmetic

--- include/simd/simd.hpp
+++ include/simd/simd.hpp
@@ -2,5 +2,48 @@
 #define SIMD_HPP_al9nn6
 
 #include <simd/common.hpp>
+#include <simd/feature_check.hpp>
 
-#endif
+namespace vector_simd
+{
+
+constexpr int kVersionMajor = 0;
+constexpr int kVersionMinor = 1;
+constexpr int kVersionPatch = 0;
+
+constexpr size_t kSSEAlignment = 16;
+constexpr size_t kAVXAlignment = 32;
+constexpr size_t kAVX512Alignment = 64;
+
+constexpr size_t kDefaultAlignment =
+    simd::compile_time::has<simd::Feature::AVX512F>() ? kAVX512Alignment
+    : simd::compile_time::has<simd::Feature::AVX>()   ? kAVXAlignment
+                                                      : kSSEAlignment;
+
+template <typename T>
+concept SimdArithmetic =
+    std::is_same_v<T, float> || std::is_same_v<T, double> ||
+    std::is_same_v<T, int8_t> || std::is_same_v<T, uint8_t> ||
+    std::is_same_v<T, int16_t> || std::is_same_v<T, uint16_t> ||
+    std::is_same_v<T, int32_t> || std::is_same_v<T, uint32_t> ||
+    std::is_same_v<T, int64_t> || std::is_same_v<T, uint64_t>;
+
+template <typename T>
+concept SimdFloat = std::is_same_v<T, float> || std::is_same_v<T, double>;
+
+template <typename T>
+concept SimdInteger = SimdArithmetic<T> && !SimdFloat<T>;
+
+template <typename T>
+concept SimdSignedInteger =
+    std::is_same_v<T, int8_t> || std::is_same_v<T, int16_t> ||
+    std::is_same_v<T, int32_t> || std::is_same_v<T, int64_t>;
+
+template <typename T>
+concept SimdUnsignedInteger =
+    std::is_same_v<T, uint8_t> || std::is_same_v<T, uint16_t> ||
+    std::is_same_v<T, uint32_t> || std::is_same_v<T, uint64_t>;
+
+} // namespace vector_simd
+
+#endif /* End of include guard: SIMD_HPP_al9nn6 */
