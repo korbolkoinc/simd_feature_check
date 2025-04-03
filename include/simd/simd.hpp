4d46d0b78e9192d5120a415d680aab093a6b1c7b
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-04-03 18:44:46 +0300

    Add type aliases for common fixed-size and native-width vector types

--- include/simd/simd.hpp
+++ include/simd/simd.hpp
@@ -1320,6 +1320,41 @@ public:
     }
 };
 
+// Type aliases for common vector types
+// Fixed-size vector types
+template <size_t N>
+using float_v = Vector<float, N>;
+template <size_t N>
+using double_v = Vector<double, N>;
+template <size_t N>
+using int8_v = Vector<int8_t, N>;
+template <size_t N>
+using uint8_v = Vector<uint8_t, N>;
+template <size_t N>
+using int16_v = Vector<int16_t, N>;
+template <size_t N>
+using uint16_v = Vector<uint16_t, N>;
+template <size_t N>
+using int32_v = Vector<int32_t, N>;
+template <size_t N>
+using uint32_v = Vector<uint32_t, N>;
+template <size_t N>
+using int64_v = Vector<int64_t, N>;
+template <size_t N>
+using uint64_v = Vector<uint64_t, N>;
+
+// Native-width vector types (width depends on best available instruction set)
+using float_vn = float_v<detail::native_width<float>::value>;
+using double_vn = double_v<detail::native_width<double>::value>;
+using int8_vn = int8_v<detail::native_width<int8_t>::value>;
+using uint8_vn = uint8_v<detail::native_width<uint8_t>::value>;
+using int16_vn = int16_v<detail::native_width<int16_t>::value>;
+using uint16_vn = uint16_v<detail::native_width<uint16_t>::value>;
+using int32_vn = int32_v<detail::native_width<int32_t>::value>;
+using uint32_vn = uint32_v<detail::native_width<uint32_t>::value>;
+using int64_vn = int64_v<detail::native_width<int64_t>::value>;
+using uint64_vn = uint64_v<detail::native_width<uint64_t>::value>;
+
 } // namespace vector_simd
 
 #endif /* End of include guard: SIMD_HPP_al9nn6 */
