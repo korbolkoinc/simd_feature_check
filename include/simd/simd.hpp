5ba0513c1306ebaea5719a6187abeeb0fe7a4220
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-07-12 22:40:46 +0300

    refactor: Move SIMD concepts to a separate header file

--- include/simd/simd.hpp
+++ include/simd/simd.hpp
@@ -5,8 +5,9 @@
 #include <bit>
 #include <cmath>
 #include <simd/common.hpp>
-#include <simd/feature_check.hpp>
+#include <simd/core/concepts.hpp>
 #include <simd/core/types.hpp>
+#include <simd/feature_check.hpp>
 
 namespace vector_simd
 {
@@ -14,27 +15,6 @@ constexpr size_t kDefaultAlignment =
     simd::compile_time::has<simd::Feature::AVX512F>() ? kAVX512Alignment
     : simd::compile_time::has<simd::Feature::AVX>()   ? kAVXAlignment
                                                       : kSSEAlignment;
-
-template <typename T>
-concept SimdArithmetic =
-    std::is_same_v<T, float> || std::is_same_v<T, double> || std::is_same_v<T, int8_t> ||
-    std::is_same_v<T, uint8_t> || std::is_same_v<T, int16_t> || std::is_same_v<T, uint16_t> ||
-    std::is_same_v<T, int32_t> || std::is_same_v<T, uint32_t> || std::is_same_v<T, int64_t> ||
-    std::is_same_v<T, uint64_t>;
-
-template <typename T>
-concept SimdFloat = std::is_same_v<T, float> || std::is_same_v<T, double>;
-
-template <typename T>
-concept SimdInteger = SimdArithmetic<T> && !SimdFloat<T>;
-
-template <typename T>
-concept SimdSignedInteger = std::is_same_v<T, int8_t> || std::is_same_v<T, int16_t> ||
-                            std::is_same_v<T, int32_t> || std::is_same_v<T, int64_t>;
-
-template <typename T>
-concept SimdUnsignedInteger = std::is_same_v<T, uint8_t> || std::is_same_v<T, uint16_t> ||
-                              std::is_same_v<T, uint32_t> || std::is_same_v<T, uint64_t>;
 
 // Forward declarations
 template <SimdArithmetic T, size_t N>
