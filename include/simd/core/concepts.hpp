eb259f7aeaeef92e64a93bb32abc13f3b83ab05c
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-07-12 22:43:48 +0300

    Wrap concepts in vector_simd namespace

--- include/simd/core/concepts.hpp
+++ include/simd/core/concepts.hpp
@@ -4,6 +4,8 @@
 #include <cstdint>
 #include <type_traits>
 
+namespace vector_simd
+{
 template <typename T>
 concept SimdArithmetic =
     std::is_same_v<T, float> || std::is_same_v<T, double> || std::is_same_v<T, int8_t> ||
@@ -25,4 +27,5 @@ template <typename T>
 concept SimdUnsignedInteger = std::is_same_v<T, uint8_t> || std::is_same_v<T, uint16_t> ||
                               std::is_same_v<T, uint32_t> || std::is_same_v<T, uint64_t>;
 
+} // namespace vector_simd
 #endif // End of include guard: LIB_SIMD_CORE_TYPES_HPP_czk5dm
