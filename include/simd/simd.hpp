3e028e8b495f032b623b09f13af598ec525f031d
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-07-12 11:43:31 +0300

    Add sin method to vector_ops for AVX support

--- include/simd/simd.hpp
+++ include/simd/simd.hpp
@@ -3982,6 +3982,31 @@ struct vector_ops<T, N, std::enable_if_t<simd::FeatureDetector<simd::Feature::AV
             }
         }
     }
+
+    template <typename U = T, std::enable_if_t<std::is_floating_point_v<U>, int> = 0>
+    static SIMD_INLINE void sin(register_t* dst, const register_t* src)
+    {
+        if constexpr (std::is_same_v<T, float>)
+        {
+            alignas(32) float tmp[8];
+            _mm256_store_ps(tmp, *src);
+            for (int i = 0; i < 8; ++i)
+            {
+                tmp[i] = std::sin(tmp[i]);
+            }
+            *dst = _mm256_load_ps(tmp);
+        }
+        else if constexpr (std::is_same_v<T, double>)
+        {
+            alignas(32) double tmp[4];
+            _mm256_store_pd(tmp, *src);
+            for (int i = 0; i < 4; ++i)
+            {
+                tmp[i] = std::sin(tmp[i]);
+            }
+            *dst = _mm256_load_pd(tmp);
+        }
+    }
 };
 #endif // SIMD_AVX
 
