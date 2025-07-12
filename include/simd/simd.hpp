98f28c06387502918f5a37cc25fd241d45866f3a
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-07-12 22:45:32 +0300

    refactor: Move SIMD architecture tags to a new header file

--- include/simd/simd.hpp
+++ include/simd/simd.hpp
@@ -4,10 +4,12 @@
 #include <array>
 #include <bit>
 #include <cmath>
+#include <simd/arch/tags.hpp>
 #include <simd/common.hpp>
 #include <simd/core/concepts.hpp>
 #include <simd/core/types.hpp>
 #include <simd/feature_check.hpp>
+
 
 namespace vector_simd
 {
@@ -26,71 +28,8 @@ class Mask;
 namespace detail
 {
 
-struct generic_tag
-{
-};
 
-struct sse2_tag : generic_tag
-{
-};
 
-struct sse3_tag : sse2_tag
-{
-};
-
-struct ssse3_tag : sse3_tag
-{
-};
-
-struct sse4_1_tag : ssse3_tag
-{
-};
-
-struct sse4_2_tag : sse4_1_tag
-{
-};
-
-struct avx_tag : sse4_2_tag
-{
-};
-
-struct avx2_tag : avx_tag
-{
-};
-
-struct avx512_tag : avx2_tag
-{
-};
-
-struct neon_tag : generic_tag
-{
-};
-
-struct wasm_simd_tag : generic_tag
-{
-};
-
-struct best_available_tag
-{
-    using type = std::conditional_t<
-        simd::compile_time::has<simd::Feature::AVX512F>(), avx512_tag,
-        std::conditional_t<
-            simd::compile_time::has<simd::Feature::AVX2>(), avx2_tag,
-            std::conditional_t<
-                simd::compile_time::has<simd::Feature::AVX>(), avx_tag,
-                std::conditional_t<
-                    simd::compile_time::has<simd::Feature::SSE42>(), sse4_2_tag,
-                    std::conditional_t<
-                        simd::compile_time::has<simd::Feature::SSE41>(), sse4_1_tag,
-                        std::conditional_t<
-                            simd::compile_time::has<simd::Feature::SSSE3>(), ssse3_tag,
-                            std::conditional_t<
-                                simd::compile_time::has<simd::Feature::SSE3>(), sse3_tag,
-                                std::conditional_t<simd::compile_time::has<simd::Feature::SSE2>(),
-                                                   sse2_tag, generic_tag>>>>>>>>;
-};
-
-using current_isa = typename best_available_tag::type;
 
 template <typename T, typename ISA>
 struct simd_width;
